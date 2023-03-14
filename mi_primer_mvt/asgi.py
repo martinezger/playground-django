9e3923467294492bb20837a703ed854e6e45be58
Germán Martinez <germand_m@hotmail.com>
2023-03-14 10:44:07 -0300

    Clase 17

---
+++ mi_primer_mvt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mi_primer_mvt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_primer_mvt.settings')
+
+application = get_asgi_application()
